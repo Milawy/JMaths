e3a190a9c9757b96fd236fa063b1abeacef7c3f0
Victor Jung <victor.jung@isen.yncrea.fr>
2020-04-08 15:12:04 +0200

    Allow the function solver to solve multiple layer functions (Exemple : f(x)=g(x), g(x)=h(x) and h(x)=42 )

--- src/JMaths/FunctionSolver.java
+++ src/JMaths/FunctionSolver.java
@@ -12,12 +12,18 @@ import java.util.regex.Pattern;
 public class FunctionSolver {
 
     private static Pattern functionPattern, functionNamePattern, functionVariablePattern;
+    private String result;
+
+    // This bool will allow interlocked functions (example f(x)=g(x) and g(x)=h(x))
+    public boolean isFinished;
 
     public FunctionSolver(){
 
         functionPattern = Pattern.compile("[a-zA-Z]+\\([a-zA-Z]+\\)");
         functionNamePattern = Pattern.compile("^[a-zA-Z]+");
         functionVariablePattern = Pattern.compile("\\([a-zA-Z]+");
+
+        this.isFinished = false;
     }
 
     private Integer find(ObservableList<String> list, String val) {
@@ -29,9 +35,24 @@ public class FunctionSolver {
         return -1;
     }
 
+    private String getResult(){
+        return this.result;
+    }
+
     public void solveFunctions(String rawString, TableColumn<Function, String> fctNameCol, TableColumn<Function, String> fctVarCol, TableColumn<Function, String> fctExpCol, TableView<Function> tableView){
 
+        this.result = rawString;
+        boolean b = this.solveFunctionsRecursive(this.result , fctNameCol, fctVarCol, fctExpCol, tableView);
+        while(!b){
+            System.out.println(this.result);
+            b = this.solveFunctionsRecursive(this.result , fctNameCol, fctVarCol, fctExpCol, tableView);
+        }
+    }
+
+    public boolean solveFunctionsRecursive(String rawString, TableColumn<Function, String> fctNameCol, TableColumn<Function, String> fctVarCol, TableColumn<Function, String> fctExpCol, TableView<Function> tableView){
+
         Matcher functionMatcher;
+        this.isFinished = true;
 
         functionMatcher = functionPattern.matcher(rawString);
 
@@ -53,8 +74,11 @@ public class FunctionSolver {
 
         // fill the list with the detected functions
         while(functionMatcher.find()){
-                System.out.println(functionMatcher.group());
                 nameFounded.add(functionMatcher.group());
+        }
+
+        if(nameFounded.size() != 0){
+            this.isFinished = false;
         }
 
         nameFounded.forEach((elt) -> {
@@ -68,14 +92,17 @@ public class FunctionSolver {
                 fctName = functionNameMatcher.group();
                 Integer id = find(nameList, fctName);
 
-                if(id != -1){
-
+                if(id != -1){ // In replaceAll function you give a regex so we have to add backslash before parenthesis 
+                    elt = elt.replaceAll("\\(", "\\\\(");
+                    elt = elt.replaceAll("\\)", "\\\\)");
+                    this.result = result.replaceFirst(elt, expressionList.get(id));
                 }
-
             }
             else{
                 System.out.println("Error in the function name");
             }
         });
+
+        return this.isFinished;
     }
 }
