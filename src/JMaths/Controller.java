7860d7d1bf641fa305163c4eb619699cabfb0de8
Victor Jung <victor.jung@isen.yncrea.fr>
2020-05-18 18:36:46 +0200

    Dynamic resolution and windowed mode done

--- src/JMaths/Controller.java
+++ src/JMaths/Controller.java
@@ -7,9 +7,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,6 +32,11 @@ public class Controller implements Initializable {
     public TextArea printArea;
     @FXML
     public LineChart lineChart;
+    @FXML
+    public Text appName, fctListName, varListName;
+    @FXML
+    public VBox vBox;
+
 
     // Define Analyser/Solver classes + all TableColumns we need
     Analyser analyser = new Analyser();
@@ -41,6 +52,98 @@ public class Controller implements Initializable {
 
     ObservableList<String> historyList = FXCollections.observableArrayList();
     Integer historyId = -1;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        // Command line listeners
+        commandLine.setOnKeyReleased(event -> {
+            if(event.getCode() == KeyCode.ENTER) { manageTab(); }
+            else if(event.getCode() == KeyCode.UP) { climbUpHistory(); }
+            else if(event.getCode() == KeyCode.DOWN) { climbDownHistory(); }
+        });
+
+        // When pressing send button
+        sendBtn.setOnAction(actionEvent -> manageTab());
+
+        // All clear buttons
+        clearPromptBtn.setOnAction(actionEvent -> printArea.setText(""));
+        clearFctBtn.setOnAction(actionEvent -> fctList.getItems().clear());
+        clearVarBtn.setOnAction(actionEvent -> varList.getItems().clear());
+        clearPlotBtn.setOnAction(actionEvent -> lineChart.getData().clear());
+    }
+
+    private void setFlexibleSize(){
+
+        // Flex size (old : 1300x800)
+        GraphicsDevice Gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        double w = Gd.getDisplayMode().getWidth();
+        double h = Gd.getDisplayMode().getHeight();
+
+        // Here you can uncomment and change the resolution to test
+        //w = 1600;
+        //h = 800;
+
+        // Flex position
+        appName.setLayoutX(w*(58.0/1300.0));
+        appName.setLayoutY(h*(55.0/800.0));
+
+        fctListName.setLayoutX(w*(1042.0/1300.0));
+        fctListName.setLayoutY(h*(55.0/800.0));
+        varListName.setLayoutX(w*(1042.0/1300.0));
+        varListName.setLayoutY(h*(395.0/800.0));
+        fctList.setLayoutX(w*(921.0/1300.0));
+        fctList.setLayoutY(h*(67.0/800.0));
+        varList.setLayoutX(w*(921.0/1300.0));
+        varList.setLayoutY(h*(407.0/800.0));
+        clearFctBtn.setLayoutX(w*(1170.0/1300.0));
+        clearFctBtn.setLayoutY(h*(356.0/800.0));
+        clearVarBtn.setLayoutX(w*(1170.0/1300.0));
+        clearVarBtn.setLayoutY(h*(690.0/800.0));
+
+        commandLine.setLayoutX(w*(58.0/1300.0));
+        commandLine.setLayoutY(h*(107.0/800.0));
+        sendBtn.setLayoutX(w*(58.0/1300.0));
+        sendBtn.setLayoutY(h*(157.0/800.0));
+        clearPromptBtn.setLayoutX(w*(744.0/1300.0));
+        clearPromptBtn.setLayoutY(h*(157.0/800.0));
+        clearPlotBtn.setLayoutX(w*(744.0/1300.0));
+        clearPlotBtn.setLayoutY(h*(690.0/800.0));
+        printArea.setLayoutX(w*(58.0/1300.0));
+        printArea.setLayoutY(h*(189.0/800.0));
+        lineChart.setLayoutX(w*(60.0/1300.0));
+        lineChart.setLayoutY(h*(340.0/800.0));
+
+        // Flex size
+        commandLine.setPrefWidth(w*(800.0/1300.0));
+        commandLine.setPrefHeight(h*(42.0/800.0));
+        fctList.setPrefWidth(w*(333.0/1300.0));
+        fctList.setPrefHeight(h*(280.0/800.0));
+        varList.setPrefWidth(w*(333.0/1300.0));
+        varList.setPrefHeight(h*(280.0/800.0));
+        printArea.setPrefWidth(w*(800.0/1300.0));
+        printArea.setPrefHeight(h*(142.0/800.0));
+        lineChart.setPrefWidth(w*(800.0/1300.0));
+        lineChart.setPrefHeight(h*(350.0/800.0));
+
+        clearFctBtn.setPrefWidth(w*(84.0/1300.0));
+        clearFctBtn.setPrefHeight(h*(21.0/800.0));
+        clearVarBtn.setPrefWidth(w*(84.0/1300.0));
+        clearVarBtn.setPrefHeight(h*(21.0/800.0));
+        clearVarBtn.setPrefWidth(w*(84.0/1300.0));
+        clearVarBtn.setPrefHeight(h*(21.0/800.0));
+        clearPromptBtn.setPrefWidth(w*(84.0/1300.0));
+        clearPromptBtn.setPrefHeight(h*(21.0/800.0));
+        sendBtn.setPrefWidth(w*(84.0/1300.0));
+        sendBtn.setPrefHeight(h*(21.0/800.0));
+
+        // Creating columns
+        fctList.getColumns().add(fctName);
+        fctList.getColumns().add(fctVar);
+        fctList.getColumns().add(fctExp);
+        varList.getColumns().add(varName);
+        varList.getColumns().add(varValue);
+    }
 
     // The one that is creating the Table rows
     private void manageTab(){
@@ -115,30 +218,4 @@ public class Controller implements Initializable {
         }
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        // Creating columns
-        fctList.getColumns().add(fctName);
-        fctList.getColumns().add(fctVar);
-        fctList.getColumns().add(fctExp);
-        varList.getColumns().add(varName);
-        varList.getColumns().add(varValue);
-
-        // Command line listeners
-        commandLine.setOnKeyReleased(event -> {
-            if(event.getCode() == KeyCode.ENTER) { manageTab(); }
-            else if(event.getCode() == KeyCode.UP) { climbUpHistory(); }
-            else if(event.getCode() == KeyCode.DOWN) { climbDownHistory(); }
-        });
-
-        // When pressing send button
-        sendBtn.setOnAction(actionEvent -> manageTab());
-
-        // All clear buttons
-        clearPromptBtn.setOnAction(actionEvent -> printArea.setText(""));
-        clearFctBtn.setOnAction(actionEvent -> fctList.getItems().clear());
-        clearVarBtn.setOnAction(actionEvent -> varList.getItems().clear());
-        clearPlotBtn.setOnAction(actionEvent -> lineChart.getData().clear());
-    }
 }
--- src/JMaths/Main.java
+++ src/JMaths/Main.java
@@ -2,19 +2,70 @@ package JMaths;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.awt.*;
 
 public class Main extends Application {
 
+    int screenWidth = (int) Screen.getPrimary().getBounds().getWidth();
+    int screenHeight = (int) Screen.getPrimary().getBounds().getHeight();
+
+    Stage stage;
+    Scene scene;
+
+    int initialX;
+    int initialY;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        GraphicsDevice Gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int width = Gd.getDisplayMode().getWidth();
+        int height = Gd.getDisplayMode().getHeight();
+
+        // Here you can uncomment and change the resolution to test
+        //width = 1600;
+        //height = 800;
+
         Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+        root.setStyle("-fx-background-color:rgb(231,216,201);");
+
         primaryStage.setTitle("This window is not allowed in your country");
         primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("LUL.png")));
-        primaryStage.setScene(new Scene(root, 1300, 800));
+
+        stage = new Stage();
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene = new Scene(root, width, height);
+
+        // Moving
+        scene.setOnMousePressed(m -> {
+            if (m.getButton() == MouseButton.PRIMARY) {
+                scene.setCursor(Cursor.MOVE);
+                initialX = (int) (stage.getX() - m.getScreenX());
+                initialY = (int) (stage.getY() - m.getScreenY());
+            }
+        });
+
+        scene.setOnMouseDragged(m -> {
+            if (m.getButton() == MouseButton.PRIMARY) {
+                stage.setX(m.getScreenX() + initialX);
+                stage.setY(m.getScreenY() + initialY);
+            }
+        });
+
+        scene.setOnMouseReleased(m -> {
+            scene.setCursor(Cursor.DEFAULT);
+        });
+
+        primaryStage.setScene(scene); // old : 1300x800
         primaryStage.show();
     }
 
