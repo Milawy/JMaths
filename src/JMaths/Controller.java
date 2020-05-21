513e63de18e13eccc8100d4f9f41eef8528e6175
Milawy <william.pecqueux@isen.yncrea.fr>
2020-05-21 15:01:14 +0200

    Forgot to push clear buttons

--- src/JMaths/Controller.java
+++ src/JMaths/Controller.java
@@ -19,6 +19,7 @@ import javafx.scene.text.Text;
 
 import java.awt.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -42,8 +43,6 @@ public class Controller implements Initializable {
 
     // Define Analyser/Solver classes + all TableColumns we need
     Analyser analyser = new Analyser();
-    FunctionSolver fctSolver = new FunctionSolver();
-    VariableSolver varSolver = new VariableSolver();
 
     TableColumn<Function, String> fctName = new TableColumn<>("Name");
     TableColumn<Function, String> fctVar = new TableColumn<>("Variable");
@@ -65,29 +64,96 @@ public class Controller implements Initializable {
         // If analyser.setRawString() returns a function
         if (analyser.isFunction){
 
-            //Converts Object to Function
+            // Counting rows
+            int i = 0;
+
+            // Converts Object to Function
             Function fctTyped = (Function) objectTyped;
 
-            //Adding the function, its variable and its expression typed by the user in the TableView
-            fctName.setCellValueFactory(new PropertyValueFactory<>("name"));
-            fctVar.setCellValueFactory(new PropertyValueFactory<>("variable"));
-            fctExp.setCellValueFactory(new PropertyValueFactory<>("expression"));
+            // If it's the first time we add something to this TableView
+            if(fctList.getItems().isEmpty()){
 
-            fctList.getItems().add(fctTyped);
+                // Adding the function, its variable and its expression typed by the user in the TableView
+                fctName.setCellValueFactory(new PropertyValueFactory<>("name"));
+                fctVar.setCellValueFactory(new PropertyValueFactory<>("variable"));
+                fctExp.setCellValueFactory(new PropertyValueFactory<>("expression"));
+                fctList.getItems().add(fctTyped);
+
+            }
+
+            // If not
+            else if(!fctList.getItems().isEmpty()){
+
+                // Checking if fctTyped is not already in TableView
+                TableColumn<Function, String> columnToCheck = fctName;
+
+                // Browsing our TableView
+                for (Function item : fctList.getItems()) {
+
+                    // If the function we typed is already in the TableView, replace its variable and expression
+                    if(fctTyped.getName().equals(columnToCheck.getCellObservableValue(item).getValue())){
+
+                        fctList.getItems().set(i, fctTyped);
+                        commandLine.clear();
+                        return;
+
+                    }
+                    i++;
+                }
+
+                // We didn't find the same function name, so create a new row
+                fctName.setCellValueFactory(new PropertyValueFactory<>("name"));
+                fctVar.setCellValueFactory(new PropertyValueFactory<>("variable"));
+                fctExp.setCellValueFactory(new PropertyValueFactory<>("expression"));
+                fctList.getItems().add(fctTyped);
+
+            }
         }
 
         // If analyser.setRawString() returns a variable
         else if (analyser.isVariable){
 
-            //Converts Object to Variable
+            // Counting rows
+            int i = 0;
+
+            // Converts Object to Variable
             Variable varTyped = (Variable) objectTyped;
-            varSolver.solveVariable(varTyped.getValue(), varName, varValue, varList);
 
-            //Adding the variable and its value typed by the user in the TableView
-            varName.setCellValueFactory(new PropertyValueFactory<>("name"));
-            varValue.setCellValueFactory(new PropertyValueFactory<>("value"));
+            // If it's the first time we add something to this TableView
+            if(varList.getItems().isEmpty()){
 
-            varList.getItems().add(varTyped);
+                varName.setCellValueFactory(new PropertyValueFactory<>("name"));
+                varValue.setCellValueFactory(new PropertyValueFactory<>("value"));
+                varList.getItems().add(varTyped);
+
+            }
+
+            // If not
+            else if(!varList.getItems().isEmpty()){
+
+                // Checking if varTyped is not already in TableView
+                TableColumn<Variable, String> columnToCheck = varName;
+
+                // Browsing our TableView
+                for (Variable item : varList.getItems()) {
+
+                    // If the variable we typed is already in the TableView, replace its value
+                    if(varTyped.getName().equals(columnToCheck.getCellObservableValue(item).getValue())){
+
+                        varList.getItems().set(i, varTyped);
+                        commandLine.clear();
+                        return;
+
+                    }
+                    i++;
+                }
+
+                // We didn't find the same variable name, so create a new row
+                varName.setCellValueFactory(new PropertyValueFactory<>("name"));
+                varValue.setCellValueFactory(new PropertyValueFactory<>("value"));
+                varList.getItems().add(varTyped);
+
+            }
         }
 
         // If analyser.setRawString() returns a printFct
@@ -96,14 +162,17 @@ public class Controller implements Initializable {
             //Converts Object to PrintFct
             PrintFct printLine = (PrintFct) objectTyped;
             printArea.appendText(printLine.solve(fctName, fctVar, fctExp, fctList, varName, varValue, varList) + "\n");
+
         }
 
+        // If analyser.setRawString() returns a plotFct
         else if(analyser.isPlot){
 
             //Converts Object to PlotFct
             PlotFct plotLine = (PlotFct) objectTyped;
             XYChart.Series<Double, Double> series = plotLine.getSerie(fctName, fctVar, fctExp, fctList, varName, varValue, varList, xAxis);
             lineChart.getData().add(series);
+
         }
 
         //TODO : Problem when returning a String, that is announcing an error in the typed text by the user.
